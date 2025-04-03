560efe4bd07a82e6eaf2e5aad598b2baeeaf60a8
Arnab-Saha-Anik <aniksahacomilla0@gmail.com>
2025-04-03 14:43:08 +0600

    server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 require("dotenv").config(); // Load environment variables
 const express = require("express");
+const cors = require("cors"); // Import cors middleware
 const connectDB = require("./config/database"); // Import the database connection
-const userRoutes = require("./controllers/userController"); // Import user routes
-
+const userController = require("./controllers/userController"); // Import user routes
+const bodyParser = require("body-parser");
 const app = express();
+
 app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+app.use(express.json()); // For JSON requests
+app.use(express.urlencoded({ extended: true })); // Parse FormData
 
 // Connect to MongoDB
 connectDB();
 
 // Routes
-app.use("/users", userRoutes); // Use the user routes
+app.use("/", userController); // Use the user routes
 
 // Start the server
 const PORT = process.env.PORT || 5000;
