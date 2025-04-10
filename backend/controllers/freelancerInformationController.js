e0d923cc5fabe88a18c77ca29c577e80bbf9561b
Arnab-Saha-Anik <aniksahacomilla0@gmail.com>
2025-04-11 00:04:28 +0600

    updated 2

--- backend/controllers/freelancerInformationController.js
+++ backend/controllers/freelancerInformationController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const FreelancerInformation = require("../models/freelancerInformationModel");
 const User = require("../models/userModel");
-const { verifyToken } = require("../middleware/authMiddleware"); // Import verifyToken middleware
+const { verifyToken } = require("../middleware/authMiddleware"); 
 
 
 router.get("/:userId", verifyToken, async (req, res) => {
